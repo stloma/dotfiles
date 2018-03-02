f71965296ff683fcdcefec4f64630699196814d0
lockwood <lockwood@fastmail.com>
2018-03-02 10:01:02 -0800

    added full paths for symbolic links

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 mkdir -p ~/.cache/vim
+mkdir -p ~/.config/vundle/bundle
 mkdir -p ~/.config/tmux
 
-ln -s vim/vimrc ~/.vimrc
-ln -s vim/vim ~/.vim
-ln -s zsh/zsh ~/.zsh
-ln -s zsh/zshrc ~/.zshrc
-ln -s tmux/tmux.conf ~/.tmux.conf
-ln -s nvim ~/.config/nvim
-ln -s ranger/config ~/.config/ranger
+ln -s $PWD/vim/vimrc ~/.vimrc
+ln -s $PWD/vim/vim ~/.vim
+ln -s $PWD/zsh/zsh ~/.zsh
+ln -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
+ln -s $PWD/tmux/plugins ~/.config/tmux 
+ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/ranger/config ~/.config/ranger
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/vundle/bundle/Vundle.vim
