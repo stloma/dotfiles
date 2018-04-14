67cb8c58b4033bb05ca2b55402f9a531a8eeba39
Stephen Martin <lockwood@opperline.com>
2018-04-14 16:41:19 -0700

    remove duplication

--- zsh/zsh/config_files/prompt.zsh
+++ zsh/zsh/config_files/prompt.zsh
@@ -13,28 +13,28 @@ setprompt() {
   setopt prompt_subst
 
   # make some aliases for the colours: (coud use normal escap.seq's too)
-  for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
-    eval PR_$color='%{$fg[${(L)color}]%}'
-    eval BL_$color='%{$fg_bold[${(L)color}]%}'
-  done
-  PR_NO_COLOR="%{$terminfo[sgr0]%}"
+  # for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
+  #   eval PR_$color='%{$fg[${(L)color}]%}'
+  #   eval BL_$color='%{$fg_bold[${(L)color}]%}'
+  # done
+  # PR_NO_COLOR="%{$terminfo[sgr0]%}"
 
-  # Check the UID
-  if [[ $UID -ge 1000 ]]; then # normal user
-    eval PR_USER='${PR_GREEN}%n${PR_NO_COLOR}'
-    eval PR_USER_OP='${PR_GREEN}%#${PR_NO_COLOR}'
-  elif [[ $UID -eq 0 ]]; then # root
-    eval PR_USER='${PR_RED}"->"${PR_NO_COLOR}'
-    eval PR_USER_OP='${PR_RED}%#${PR_NO_COLOR}'
-  fi
+  # # Check the UID
+  # if [[ $UID -ge 1000 ]]; then # normal user
+  #   eval PR_USER='${PR_GREEN}%n${PR_NO_COLOR}'
+  #   eval PR_USER_OP='${PR_GREEN}%#${PR_NO_COLOR}'
+  # elif [[ $UID -eq 0 ]]; then # root
+  #   eval PR_USER='${PR_RED}"->"${PR_NO_COLOR}'
+  #   eval PR_USER_OP='${PR_RED}%#${PR_NO_COLOR}'
+  # fi
 
-  # Check if we are on SSH or not
-  # if [[ -n "$SSH_CLIENT"  ||  -n "$SSH2_CLIENT" ]]; then
-  if [[ -n "$SSH_CONNECTION" ]]; then
-    eval PR_HOST='${PR_YELLOW}%M${PR_NO_COLOR}' #SSH
-  else
-    eval PR_HOST='${PR_GREEN}%M${PR_NO_COLOR}' # no SSH
-  fi
+  # # Check if we are on SSH or not
+  # # if [[ -n "$SSH_CLIENT"  ||  -n "$SSH2_CLIENT" ]]; then
+  # if [[ -n "$SSH_CONNECTION" ]]; then
+  #   eval PR_HOST='${PR_YELLOW}%M${PR_NO_COLOR}' #SSH
+  # else
+  #   eval PR_HOST='${PR_GREEN}%M${PR_NO_COLOR}' # no SSH
+  # fi
   # set the prompt
   PS1=$'${PR_USER} ${BL_RED}%~ ${PR_NO_COLOR}'
   PS2=$'%_>'
