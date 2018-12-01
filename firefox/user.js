c66744594b61c24920856a6c653d027517a62937
Stephen Martin <lockwood@opperline.com>
2018-11-30 18:44:17 -0800

    initial firefox settings

---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+user_pref("widget.content.gtk-theme-override",  "Arc");
+
+user_pref("browser.search.context.loadInBackground", true);
+user_pref("browser.tabs.loadBookmarksInBackground", true);
+user_pref("browser.tabs.loadDivertedInBackground", true);
+
+user_pref("geo.enabled", false);
+user_pref("dom.webnotifications.enabled", false);
