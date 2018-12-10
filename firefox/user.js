1669879c74bb83917f658c5922bc89c9089d1122
Stephen Martin <lockwood@opperline.com>
2018-12-09 16:33:09 -0800

    additional firefox settings

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,5 @@
 user_pref("widget.content.gtk-theme-override",  "Arc");
+user_pref("layout.spellcheckDefault",  2);
 
 user_pref("browser.search.context.loadInBackground", true);
 user_pref("browser.tabs.loadBookmarksInBackground", true);
@@ -6,3 +7,12 @@ user_pref("browser.tabs.loadDivertedInBackground", true);
 
 user_pref("geo.enabled", false);
 user_pref("dom.webnotifications.enabled", false);
+
+// Printing
+user_pref("print.print_headerleft", "")
+user_pref("print.print_headerright", "")
+user_pref("print.print_footerright", "")
+user_pref("print.print_footerright", "")
+
+// Search
+user_pref("browser.search.openintab", true)
