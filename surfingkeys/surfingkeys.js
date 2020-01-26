6f49bb5e2b66942d05e45ab159b6e948a276fd87
Stephen Martin <lockwood@opperline.com>
2020-01-26 07:29:21 -0800

    add surfingkeys configuration

---
+++ surfingkeys/surfingkeys.js
@@ -0,0 +1,7 @@
+map('F', 'gf'); // open in background tab
+
+map('ob', 'b'); // open bookmark
+map('ou', 't'); // open url
+
+map('<Ctrl-d>', 'd') // scroll down 1/2 page
+map('<Ctrl-u>', 'u') // scroll up 1/2 page
