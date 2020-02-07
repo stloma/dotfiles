1e4e24a6e11c0a5394af62ccc459cac2f8ffd7ab
Stephen Martin <lockwood@opperline.com>
2020-02-07 07:15:16 -0800

    refactor hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,7 @@
 
--- Notification examples
--- hs.notify.new({title="Title Text", informativeText="Information text"}):send()
---  hs.alert.show("Config Reloaded")
-
-
+----------------------------
+-- Hammerspoon Configuration
+----------------------------
 
 local mash = {"cmd", "alt", "ctrl", "shift"}
 
@@ -11,9 +9,13 @@ hs.hotkey.bind(mash, "`", function()
   hs.reload()
 end)
 
-hs.window.animationDuration = 0
+hs.alert.show("Hammerspoon loaded")
 
--- Taken from: https://github.com/philc/hammerspoon-config/blob/master/init.lua#L87
+-------------------------
+-- Application Management
+-------------------------
+
+-- https://github.com/philc/hammerspoon-config/
 function launch_or_focus(appName)
    local app = hs.appfinder.appFromName(appName)
    if app == nil then
@@ -26,126 +28,104 @@ function launch_or_focus(appName)
    end
 end
 
---------------------
--- Open Applications
---------------------
 hs.hotkey.bind(mash, "space", function() launch_or_focus("iTerm") end)
 hs.hotkey.bind(mash, "e", function() launch_or_focus("Emacs") end)
 hs.hotkey.bind(mash, "f", function() launch_or_focus("Firefox") end)
 hs.hotkey.bind(mash, "d", function() launch_or_focus("Google Chrome") end)
 hs.hotkey.bind(mash, "s", function() launch_or_focus("Slack") end)
+hs.hotkey.bind(mash, "a", function() launch_or_focus("Spotify") end)
 hs.hotkey.bind(mash, "z", function() launch_or_focus("Pycharm") end)
 hs.hotkey.bind(mash, "x", function() launch_or_focus("IntelliJ IDEA") end)
 hs.hotkey.bind(mash, "c", function() launch_or_focus("Visual Studio Code") end)
 
-
 --------------------
 -- Window Management
 --------------------
-function in_range(width, size)
-  return width > size - 10 and width < size + 10
+
+hs.grid.setGrid('12x12')
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+hs.window.animationDuration = 0
+local lastSeenChain = nil
+local lastSeenWindow = nil
+local screenCount = #hs.screen.allScreens()
+
+local grid = {
+  rightHalf = '6,0 6x12',
+  rightThird = '8,0 4x12',
+  rightTwoThirds = '4,0 8x12',
+
+  leftHalf = '0,0 6x12',
+  leftThird = '0,0 4x12',
+  leftTwoThirds = '0,0 8x12',
+
+  fullScreen = '0,0 12x12',
+  centeredTall = '2.5,0 7x12',
+  centeredBig = '2.5,2.5 7x7',
+  centeredSmall = '3.5,3.5 5x5',
+}
+
+-- https://github.com/wincent/wincent
+function chain(movements)
+  local chainResetInterval = 2 -- seconds
+  local cycleLength = #movements
+  local sequenceNumber = 1
+
+  return function()
+    local win = hs.window.frontmostWindow()
+    local id = win:id()
+    local now = hs.timer.secondsSinceEpoch()
+    local screen = win:screen()
+
+    if
+      lastSeenChain ~= movements or
+      lastSeenAt < now - chainResetInterval or
+      lastSeenWindow ~= id
+    then
+      sequenceNumber = 1
+      lastSeenChain = movements
+    end
+    lastSeenAt = now
+    lastSeenWindow = id
+
+    hs.grid.set(win, movements[sequenceNumber], screen)
+    sequenceNumber = sequenceNumber % cycleLength + 1
+  end
 end
 
-hs.hotkey.bind(mash, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+hs.hotkey.bind(mash, 'left', chain({
+  grid.leftHalf,
+  grid.leftThird,
+  grid.leftTwoThirds,
+}))
 
-  local one_third = max.w * (1/3)
-  local half = max.w* .5
-  local two_third = max.w * (2/3)
+hs.hotkey.bind(mash, 'right', chain({
+  grid.rightHalf,
+  grid.rightThird,
+  grid.rightTwoThirds,
+}))
 
-  if in_range(f.w, one_third) then
-    f.w = two_third
-  elseif in_range(f.w, half) then
-    f.w = one_third
-  else
-    f.w = half
-  end
+hs.hotkey.bind(mash, 'up', chain({
+  grid.fullScreen,
+}))
 
-  f.x = max.x
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  local one_third = max.w * (1/3)
-  local half = max.w * .5
-  local two_third = max.w * (2/3)
-
-  if in_range(f.w, half) then
-    f.w = one_third
-    f.x = max.x + (max.w * (2/3))
-  elseif in_range(f.w, one_third) then
-    f.w = two_third
-    f.x = max.x + (max.w * (1/3))
-  else
-    f.w = half
-    f.x = max.x + (max.w * .5)
-  end
-
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "Up", function ()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local max = win:screen():frame()
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "Down", function ()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local max = win:screen():frame()
-
-  local two_third = max.w * .75
-  local one_third = max.w * .5
-
-  f.x = max.x + (max.w * .25)
-  f.y = max.y
-  f.w = one_third
-  f.h = max.h
-  win:setFrame(f)
-end)
+hs.hotkey.bind(mash, 'down', chain({
+  grid.centeredTall,
+  grid.centeredBig,
+  grid.centeredSmall,
+}))
 
 hs.hotkey.bind(mash, "=", function ()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local max = win:screen():frame()
 
-  local x_diff = f.x - max.x
-  local y_diff = f.y - max.y
-
-  -- TODO: cleanup hacky calculations
-  if y_diff > 10 then
-    f.h = f.h + 10
-    f.y = f.y - 10
-  end
-
-  if x_diff > 10 then
-    f.x = f.x - 10
-    f.w = f.w + 10
-  end
-
-  win:setFrame(f)
-
+  hs.grid.resizeWindowWider(win)
+  hs.grid.resizeWindowTaller(win)
 end)
 
 hs.hotkey.bind(mash, "-", function ()
-  -- TODO: shrink
+  local win = hs.window.focusedWindow()
+
+  hs.grid.resizeWindowThinner(win)
+  hs.grid.resizeWindowShorter(win)
 end)
 
