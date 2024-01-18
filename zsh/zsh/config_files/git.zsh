9bc80591f583aa2303f16cde7633a008df9eda55
Stephen Martin <lockwood@opperline.com>
2024-01-18 04:21:27 -0800

    do not track git status in prompt for hostname

--- zsh/zsh/config_files/git.zsh
+++ zsh/zsh/config_files/git.zsh
@@ -3,7 +3,7 @@ function git_prompt_info() {
   local ref
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  if [[ $(uname -n) == smartin.local ]]; then
+  if [[ $(uname -n) == smartin-macbookpro.local ]]; then
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
   else
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
